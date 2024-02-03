4ebb474b7b0446f050dc6e036cfffeec75abe547
samit1719 <samuelteshome1719@gmail.com>
2024-02-03 18:39:48 +0300

    updated task 4 index.js

--- 0x01-react_intro/task_4/dashboard/src/index.js
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -4,14 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Notifications from "./Notifications";
 
-
-const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
-rootNotifications.render(
-  <React.StrictMode>
-    <Notifications />
-  </React.StrictMode>
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -19,6 +11,11 @@ root.render(
   </React.StrictMode>
 );
 
-
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
 
 reportWebVitals();
