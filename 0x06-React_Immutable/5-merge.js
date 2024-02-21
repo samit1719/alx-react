8c1c8f00b1b2c9978506983f27f0a9d4b50da0fe
samit1719 <samuelteshome1719@gmail.com>
2024-02-21 15:05:25 +0300

    task 5

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,5 +1,17 @@
-import { List, Map } from 'immutable';
+import immutable from 'immutable';
 
-export const concatElements = (page1, page2) => List(page1).concat(List(page2));
+const { Map, List } = immutable;
 
-export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
+export function concatElements(page1, page2) {
+  const list1 = List(page1);
+  const list2 = List(page2);
+  const list3 = list1.concat(list2);
+  return list3;
+}
+
+export function mergeElements(page1, page2) {
+  const obj1 = Map(page1);
+  const obj2 = Map(page2);
+  const obj3 = obj1.merge(obj2);
+  return obj3;
+}
