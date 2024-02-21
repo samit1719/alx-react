e0a6777ec63e1ed066b0f78cb8c8902323eed695
samit1719 <samuelteshome1719@gmail.com>
2024-02-21 15:20:11 +0300

    task 8 update

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,24 @@
-import immutable from 'immutable';
+import { Seq } from 'immutable';
 
-const { Seq } = immutable;
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-export default function printBestStudents() {
-    
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
+
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
