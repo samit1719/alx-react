6761c11ac17d806e04709eadcf866bc3fe2eb9da
samit1719 <samuelteshome1719@gmail.com>
2024-02-21 15:06:01 +0300

    task 6

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,4 +1,6 @@
-import { Map } from 'immutable';
+import immutable from 'immutable';
+
+const { Map } = immutable;
 
 export default function mergeDeeplyElements(page1, page2) {
   return Map(page1).mergeDeep(Map(page2));
